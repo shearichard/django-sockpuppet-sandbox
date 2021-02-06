893ff902f50a6f3e2dd219fc6b3502ab3a16cc15
Richard Shea <rshea@thecubagroup.com>
2021-02-07 00:06:34 +1300

    Page now loads and initiates sockpuppet-sync network action although requests on that channel don't currently work

--- backend/todo/reflexes/todo_reflex_reflex.py
+++ backend/todo/reflexes/todo_reflex_reflex.py
@@ -4,3 +4,5 @@ from sockpuppet.reflex import Reflex
 class TodoReflexReflex(Reflex):
     def increment(self, step=1):
         self.count = int(self.element.dataset['count']) + step
+    def indicate_that_button_has_been_pushed(self):
+        print("demoofbuttonpush has fired")
